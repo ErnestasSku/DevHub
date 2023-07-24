624f0fba59afc57fd0fc455a212048defb0050ff
ErnestasSku <ernestassku@gmail.com>
2023-07-24 23:44:35 +0300

    MockSite

--- MockSite/index.js
+++ MockSite/index.js
@@ -1,22 +1,32 @@
 console.log("Hello World!");
 
-var socket = new WebSocket("ws://localhost:3030/echo");
+// var socket = new WebSocket("ws://localhost:3030/echo");
 
-socket.onopen = function () {
-  console.log("WebSocket connection established.");
+// socket.onopen = function () {
+//   console.log("WebSocket connection established.");
 
-  // You can send data to the server after the connection is open
-  socket.send("Hello, server!");
-};
+//   // You can send data to the server after the connection is open
+//   socket.send("Hello, server!");
+// };
 
-socket.onmessage = function (event) {
-  console.log("Received message from server:", event.data);
-};
+// socket.onmessage = function (event) {
+//   console.log("Received message from server:", event.data);
+// };
 
-socket.onclose = function (event) {
-  console.log("WebSocket connection closed with code:", event.code);
-};
+// socket.onclose = function (event) {
+//   console.log("WebSocket connection closed with code:", event.code);
+// };
 
-socket.onerror = function (error) {
-  console.error("WebSocket error:", error);
-};
+// socket.onerror = function (error) {
+//   console.error("WebSocket error:", error);
+// };
+
+fetch("http://127.0.0.1:3030/Task", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ a: "b" }),
+})
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
