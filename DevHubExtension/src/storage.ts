d3a6e218e842a51ffe67b32a0fb36fb846d79991
ErnestasSku <ernestassku@gmail.com>
2023-08-07 08:52:08 +0300

    Disable experimental features

--- DevHubExtension/src/storage.ts
+++ DevHubExtension/src/storage.ts
@@ -17,7 +17,7 @@ const defaultFeatures: IFeatures = {
   task_link: true,
   tauri_integration: false,
   requests: false,
-  experimental: true,
+  experimental: false,
 };
 
 export const features = {
